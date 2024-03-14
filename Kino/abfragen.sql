2b7d2f7147ce862102dcc10fb680399cfe935b13
LT:) <123622201+tl155438@users.noreply.github.com>
2024-03-14 08:03:52 +0000

    Challenges bearbeitet

--- Kino/abfragen.sql
+++ Kino/abfragen.sql
@@ -1,2 +1,26 @@
 Select*From Film;
 Select*From Regisseur;
+-- Löschender doppeltenZeilen
+DELETE FROM Regisseur
+WHERE RegisseurID >=6;   
+
+-- Insert der Filme
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('Harry Potter 2', 161, 2002, 1);
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('Aquaman', 143, 2018, 2);
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('Apocalypse Now', 153, 1979, 3);
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('Blade Runner', 117, 1982, 4);
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('The Artist', 100, 2011, 5);
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('Harry Potter 3', 142, 2004, 1);
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('The Godfather Part II', 200, 1974, 3);
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('The Godfather Part III', 162, 1990, 3);
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('Alien', 117, 1979, 4); -- obwohl es ein Sci-Fi Horrorfilm ist, könnte er aufgrund seiner historischen Bedeutung und künstlerischen Qualität für ältere Jugendliche geeignet sein
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('Gladiator', 155, 2000, 4);
---
+++ Kino/challenges.sql
@@ -0,0 +1,11 @@
+-- challange 1
+SELECT DISTINCT Jahr FROM Film
+ORDER BY Jahr
+LIMIT 1;
+
+--Challange 2
+SELECT DISTINCT Länge FROM Film
+ORDER BY Länge DESC
+LIMIT 1;
+
+--Challange 3
